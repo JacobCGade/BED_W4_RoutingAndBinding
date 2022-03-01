8deb3f65558cd17ea0e07cabfed87792d1f08ae8
Jacob Gade <jacobcgade@gmail.com>
2022-03-01 16:08:36 +0100

    Add project files.

---
+++ BED_W4_Exercise/Pages/Contact/Details.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BED_W4_Exercise.Pages.Contact
+{
+    public class DetailsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ BED_W4_Exercise/Pages/Contact/Index.cshtml.cs
@@ -0,0 +1,34 @@
+using BED_W4_Exercise.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BED_W4_Exercise.Pages.Contact
+{
+    public class IndexModel : PageModel
+    {
+        [BindProperty]
+        public Models.Contact ContactModel { get; set; }
+
+        public StoreContactInfo Service { get; set; }
+
+        public IndexModel(StoreContactInfo service)
+        {
+            Service = service;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            Service.Contacts.Add(ContactModel);
+            return Page();
+        }
+    }
+}
